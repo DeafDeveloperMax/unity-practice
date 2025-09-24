d63f920509d634ccbc353b717750773f023ca89e
Maksym Voznyi <mak7kas02@gmail.com>
2025-09-24 15:35:34 +0300

    fix car movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,46 +2,22 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    private Rigidbody rb;
-    private Vector2 moveInput;
-
-
-    private CharacterController controller;
+    public float speed = 20f;
+    public float turnSpeed = 50f;
+    
 
     void Start()
     {
-        controller = GetComponent<CharacterController>();
-    }
-
-    void FixedUpdate()
-    {
-        Vector3 movement = new Vector3(moveInput.x, 0f, moveInput.y) * moveSpeed;
-        controller.Move(movement * Time.fixedDeltaTime);
     }
     
-    private float speed = 2.0f;
-    public GameObject character;
-
     void Update () {
-        KeyCode d = KeyCode.RightArrow;
-        if (Input.GetKeyDown(d)){
-            transform.position += Vector3.right * speed * Time.deltaTime;
-        }
+        float moveInput = Input.GetAxis("Vertical"); 
 
-        KeyCode a = KeyCode.LeftArrow;
-        if (Input.GetKeyDown(a)){
-            transform.position += Vector3.left * speed * Time.deltaTime;
-        }
+        float turnInput = Input.GetAxis("Horizontal"); 
 
-        KeyCode w = KeyCode.UpArrow;
-        if (Input.GetKeyDown(w)){
-            transform.position += Vector3.forward * speed * Time.deltaTime;
-        }
 
-        KeyCode s = KeyCode.DownArrow;
-        if (Input.GetKeyDown(s)){
-            transform.position += Vector3.back * speed * Time.deltaTime;
-        }
+        transform.Translate(Vector3.forward * moveInput * speed * Time.deltaTime);
+
+        transform.Rotate(Vector3.up * turnInput * turnSpeed * Time.deltaTime);
     }
 }
