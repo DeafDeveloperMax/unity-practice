54d623a2216f4bc48401f9888cf5ecf1437614b1
Maksym Voznyi <mak7kas02@gmail.com>
2025-09-24 16:47:45 +0300

    little fix speed

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,21 +3,18 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 20f;
-    public float turnSpeed = 50f;
+    public float turnSpeed = 60f;
     
-
     void Start()
     {
+        
     }
     
     void Update () {
         float moveInput = Input.GetAxis("Vertical"); 
-
         float turnInput = Input.GetAxis("Horizontal"); 
 
-
         transform.Translate(Vector3.forward * moveInput * speed * Time.deltaTime);
-
         transform.Rotate(Vector3.up * turnInput * turnSpeed * Time.deltaTime);
     }
 }
