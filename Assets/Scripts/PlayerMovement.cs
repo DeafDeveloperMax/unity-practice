b00630b69fba8832f88fbfe8d010d5add1ba8d99
Maksym Voznyi <mak7kas02@gmail.com>
2025-09-25 10:39:51 +0300

    add new hood camera add 2 player + main and hood camera fix input

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,16 +5,28 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 20f;
     public float turnSpeed = 60f;
     
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey;
+
+    public string inputID;
+    
     void Start()
     {
         
     }
     
     void Update () {
-        float moveInput = Input.GetAxis("Vertical"); 
-        float turnInput = Input.GetAxis("Horizontal"); 
+        float moveInput = Input.GetAxis("Vertical" + inputID); 
+        float turnInput = Input.GetAxis("Horizontal" + inputID); 
 
         transform.Translate(Vector3.forward * moveInput * speed * Time.deltaTime);
         transform.Rotate(Vector3.up * turnInput * turnSpeed * Time.deltaTime);
+
+        if (Input.GetKeyDown(switchKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+        }
     }
 }
