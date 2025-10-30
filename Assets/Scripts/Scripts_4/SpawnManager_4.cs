92cc43ae37685238119244ac3e887cfd20c91458
Maksym Voznyi <mak7kas02@gmail.com>
2025-10-30 14:12:30 +0200

    [+] Spawn Enemy and Power

---
+++ Assets/Scripts/Scripts_4/SpawnManager_4.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class SpawnManager_4 : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public GameObject powerupPrefab;
+    private float spawnRange = 9.0f;
+    public int enemyCount;
+    public int waveNumber = 1;
+    
+    void Start()
+    {
+        SpawnEnemyWake(waveNumber);
+        Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+    }
+
+    
+    void Update()
+    {
+        enemyCount = FindObjectsByType<Enemy>(FindObjectsSortMode.None).Length;
+        if (enemyCount == 0)
+        {
+            waveNumber++;
+            SpawnEnemyWake(waveNumber);
+            Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+        }
+    }
+
+    void SpawnEnemyWake(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        { 
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
+    }
+
+    private Vector3 GenerateSpawnPosition()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+        Vector3 spawnPos = new Vector3(spawnPosX, 0, spawnPosZ);
+        
+        return spawnPos;
+    }
+}
