69d96796b3be08dbe21da37c1ea02d2ff8accbd8
Maksym Voznyi <mak7kas02@gmail.com>
2025-11-10 11:11:36 +0200

    fix horizontal

--- Assets/Challenge 4/Scripts/RotateCameraX4.cs
+++ Assets/Challenge 4/Scripts/RotateCameraX4.cs
@@ -10,10 +10,9 @@ public class RotateCameraX4 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
+        float horizontalInput = Input.GetAxis("Horizontal1");
         transform.Rotate(Vector3.up, horizontalInput * speed * Time.deltaTime);
 
         transform.position = player.transform.position; // Move focal point with player
-
     }
 }
