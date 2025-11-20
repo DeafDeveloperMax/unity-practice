3b990924747e6cc1f39fd89cca3487662c0da741
Maksym Voznyi <mak7kas02@gmail.com>
2025-11-20 14:04:15 +0200

    [+] prefabs [+] random spawn + position target [+] Click Mouse1 (left click) to destroy

---
+++ Assets/Scripts/Scripts_5/Target_5.cs
@@ -0,0 +1,51 @@
+using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Target_5 : MonoBehaviour
+{
+    private Rigidbody targetRb;
+    private float minSpeed = 12f;
+    private float maxSpeed = 16f;
+    private float maxTorque = 10f;
+    private float xRange = 4f;
+    private float ySpawnPos = -6f;
+    
+    void Start()
+    {
+        targetRb = GetComponent<Rigidbody>();
+        targetRb.AddForce(RandomForce(), ForceMode.Impulse);
+        targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+        transform.position = RandomSpawnPos();
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+    
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+
+    float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+
+    Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
+    }
+}
